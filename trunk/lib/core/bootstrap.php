656e00cf2ce9348a0f2ecc22c85d5e8b805a24f0
qq19831030qq@gmail.com <qq19831030qq@gmail.com@e64d0df5-b125-41e6-6fc9-d978c2b1daaa>
2012-09-29 16:59:59 +0000

    make boot

--- trunk/lib/core/bootstrap.php
+++ trunk/lib/core/bootstrap.php
@@ -1,25 +1,15 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
+define('PS', PATH_SEPARATOR);
 define('DHC_VERSIONs', '0.1');
-define('DHC_LIB', dirname(__FILE__) . DIRECTORY_SEPARATOR);
-define('ROOT_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR);
-#define("STATICPATH", dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . "static.tripbe.com" . DIRECTORY_SEPARATOR);
+define('DHC_LIB', dirname(dirname(__FILE__)) . DS);
+define('DHC_CONF', dirname(dirname(dirname(__FILE__))) . DS . 'conf' . DS);
+set_include_path( get_include_path() . PS . DHC_LIB . PS . DHC_CONF);
 
-include( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'function.php' );
-include( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'controller.php' );
-include( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'db.php' );
-include( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'pager.php' );
-include( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'error.php' );
-include( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PagedList.php' );
-include( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'geo.php' );
-include( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'httpsqs_client.php' );
-include 'dbmapper.php';
-include 'dbobject.php';
-include( dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'dbmapper.php' );
-include( dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'dbobject.php' );
-include( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'FormToken.php' );
 
-set_include_path( MVCPATH );
+include( DHC_LIB . 'core/function.php' );
+
+$G = array();
 
 function run(){
     echo 'run';
